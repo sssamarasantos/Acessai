a9def5b37d001a6280c04157d70ffc1b155e3ad2
Samara Santos <samara.souza2013@outlook.com>
2024-01-31 22:42:59 -0300

    criação de classes api

---
+++ Api/Acessai/Acessai.Domain/Models/Aula.cs
@@ -0,0 +1,10 @@
+﻿namespace Acessai.Domain.Models
+{
+    public class Aula : BaseModel
+    {
+        public string Nome { get; set; }
+        public DateTime DataHoraCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        public long IdProfessorDisciplina { get; set; }
+    }
+}
---
+++ Api/Acessai/Acessai.Domain/Models/Cronograma.cs
@@ -0,0 +1,12 @@
+﻿namespace Acessai.Domain.Models
+{
+    public class Cronograma : BaseModel
+    {
+        public string Data { get; set; }
+        public string Hora { get; set; }
+        public DateTime DataHoraCriacao { get; set; }
+        public DateTime DataHoraAlteracao { get; set; }
+        public long IdAluno { get; set; }
+        public long IdVideoaula { get; set; }
+    }
+}
